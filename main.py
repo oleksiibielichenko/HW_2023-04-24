15a86166fc38e2cfcb4226bef63152755e0733b6
oleksiibielichenko <byelichenko@gmail.com>
2023-04-25 23:46:30 +0300

    333

--- main.py
+++ main.py
@@ -1,10 +1,32 @@
 import unittest
 
 
-class User(self, name, age):
+class User:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
     def say_hello():
+        a = 
         return 'Hello world'
+
+
+# class Admin(User):
+#     def __init__(self, name, age):
+#         super().__init__(name, age)
+#     def mend_something():
+#         return 01101101
+
+
+
+
+
+
+user = User('Oleksii', 40)
+user.say_hello
 
 
 if __name__ == '__main__':
     unittest.main()
+
+
